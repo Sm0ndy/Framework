fa6af8ee8eae64b648807cc29f7438fd5b188dea
Sm0ndy <martmond89@gmail.com>
2025-07-29 18:23:10 +0200

    Add files via upload

---
+++ framework_webapp1.py
@@ -0,0 +1,112 @@
+Sheet: Foglio1
+[95 rows x 8 columns]
+Column names: |Macro-phase|Phase|Sub-phase|Deliverables|Responsibile Roles|Accountable Roles|Timing|Suggested Date
+0|Kick Off project|Kick Off project|Target finalization|NaN|NaN|NaN|NaN|NaN
+1|Kick Off project|Kick Off project|Lesson learned|NaN|NaN|NaN|NaN|NaN
+2|Project planning (by module)|Project planning (by module)|Project planning (by module)|NaN|NaN|NaN|NaN|NaN
+3|Project planning (by module)|Project planning (by module)|Modules validation|NaN|NaN|NaN|NaN|NaN
+4|Planning|module definition|Modules priorities and cost allocation |NaN|NaN|NaN|NaN|NaN
+5|Existing solution adaptation|existing solution adaptation|Cross-check within company, group and market |NaN|NaN|NaN|NaN|NaN
+6|New solutions design|Ideas Generation|Specs, needs and expectations review|NaN|NaN|NaN|NaN|NaN
+7|New solutions design|Ideas Generation|Components specification|NaN|NaN|NaN|NaN|NaN
+8|New solutions design|Ideas Generation|AT&I specification|NaN|NaN|NaN|NaN|NaN
+9|New solutions design|Ideas Generation|Modul strategy analysis: make, buy or co-design|NaN|NaN|NaN|NaN|NaN
+10|New solutions design|Ideas Generation|Internal & external Design review|NaN|NaN|NaN|NaN|NaN
+11|New solutions design|Machine design|Project pillars design|NaN|NaN|NaN|NaN|NaN
+12|New solutions design|Machine design| Design for standardization|NaN|NaN|NaN|NaN|NaN
+13|New solutions design|Machine design|Technical supervision based on costs control|NaN|NaN|NaN|NaN|NaN
+14|New solutions design|Machine design|FMEA Analysis |NaN|NaN|NaN|NaN|NaN
+15|New solutions design|Machine design|Machine new Bricks creations |NaN|NaN|NaN|NaN|NaN
+16|New solutions design|Machine design|Common glossary definition and standardization|NaN|NaN|NaN|NaN|NaN
+17|New solutions design|Machine design|Actual cost real time update and DFX checks|NaN|NaN|NaN|NaN|NaN
+18|New solutions design|Size parts design |Size parts prioritization |NaN|NaN|NaN|NaN|NaN
+19|New solutions design|Size parts design |Rough analysys of specs sentivity|NaN|NaN|NaN|NaN|NaN
+20|New solutions design|Digital features design (Kinematic Analysis)|Features/Parameters definition|NaN|NaN|NaN|NaN|NaN
+21|New solutions design|Digital features design (Kinematic Analysis)|Process optimization|NaN|NaN|NaN|NaN|NaN
+22|New solutions design|Pneumatic, vacuum hydraulic and electrical system design|Guidleline definition |NaN|NaN|NaN|NaN|NaN
+23|New solutions design|Pneumatic, vacuum hydraulic and electrical system design|Assembly phase optimization|NaN|NaN|NaN|NaN|NaN
+24|New solutions design|HW devices and cabinet design |Wiring and Testing phase optimization|NaN|NaN|NaN|NaN|NaN
+25|New solutions design|SW design|Cross-check within company and group |NaN|NaN|NaN|NaN|NaN
+26|New solutions design|SW design| Testing phase optimization|NaN|NaN|NaN|NaN|NaN
+27|New solutions design|SW design|HMI internal users experience definition|NaN|NaN|NaN|NaN|NaN
+28|New solutions design|Layout design|Standard layout definition |NaN|NaN|NaN|NaN|NaN
+29|New solutions design|Lift & Movements design|Standard jigs definition and re-use |NaN|NaN|NaN|NaN|NaN
+30|Industrialization & cost optimization|Post design activities|S&OP simulation |NaN|NaN|NaN|NaN|NaN
+31|Industrialization & cost optimization|Post design activities|Material Strategy definition|NaN|NaN|NaN|NaN|NaN
+32|Industrialization & cost optimization|Post design activities|Sequencing (rough hypothesis)|NaN|NaN|NaN|NaN|NaN
+33|Industrialization & cost optimization|Post design activities|Components risk assesment|NaN|NaN|NaN|NaN|NaN
+34|Industrialization & cost optimization|Post design activities|Cost analysis and Preliminary DTC action to be planned|NaN|NaN|NaN|NaN|NaN
+35|Industrialization & cost optimization|Post design activities|Final design review|NaN|NaN|NaN|NaN|NaN
+36|Simulation/Proof of principle/Module prototyping/back-up solution|Simulation/Proof of principle/Module prototyping/back-up solution|Machine ergonomics analysis|NaN|NaN|NaN|NaN|NaN
+37|Virtual commissioning and module design review|Virtual commissioning and module design review|Machine verification and stress for optimization|NaN|NaN|NaN|NaN|NaN
+38|System integration and verification|System integration and verification|Change Request management|NaN|NaN|NaN|NaN|NaN
+39|System integration and verification|System integration and verification|Structured Team Retrospective|NaN|NaN|NaN|NaN|NaN
+40|Documentation & Processes|Documentation & Processes|Documentation AT&I|NaN|NaN|NaN|NaN|NaN
+41|Documentation & Processes|Documentation & Processes|Operation process design|NaN|NaN|NaN|NaN|NaN
+42|Documentation & Processes|Documentation & Processes|Digital service AT&I Design |NaN|NaN|NaN|NaN|NaN
+43|Documentation & Processes|Documentation & Processes|New industrialization initiative design|NaN|NaN|NaN|NaN|NaN
+44|Preliminary BOM (engineering, assembly and services)|Preliminary BOM (engineering, assembly and services)|Ebom creation|NaN|NaN|NaN|NaN|NaN
+45|Preliminary BOM (engineering, assembly and services)|Preliminary BOM (engineering, assembly and services)|Mbom simulation + Sequencing|NaN|NaN|NaN|NaN|NaN
+46|Manufacturing|BOM analysis with SC perspective|Detailed material strategy definition|NaN|NaN|NaN|NaN|NaN
+47|Manufacturing|BOM analysis with SC perspective|Outsourcing vs insourcing|NaN|NaN|NaN|NaN|NaN
+48|Manufacturing|Drawing components purchasing|Supplier detailed agreement and strategy|NaN|NaN|NaN|NaN|NaN
+49|Manufacturing|Commercial components purchasing|Supplier detailed agreement and strategy|NaN|NaN|NaN|NaN|NaN
+50|Manufacturing|Schedule Agreement purchasing|Supplier detailed agreement and strategy|NaN|NaN|NaN|NaN|NaN
+51|Manufacturing|External pre-Assemblies purchasing|Supplier detailed agreement and strategy|NaN|NaN|NaN|NaN|NaN
+52|Manufacturing|Co - Designed groups purchasing|Supplier detailed agreement and strategy|NaN|NaN|NaN|NaN|NaN
+53|Manufacturing|Cabinet purchasing|Supplier detailed agreement and strategy|NaN|NaN|NaN|NaN|NaN
+54|Manufacturing|SC management|Supply chain quality and Risk assessment|NaN|NaN|NaN|NaN|NaN
+55|Manufacturing|Quality checks for components and group|Quality defect database creation|NaN|NaN|NaN|NaN|NaN
+56|Manufacturing|Inventory management and simulation|Inventory management and simulation|NaN|NaN|NaN|NaN|NaN
+57|Manufacturing|Materials handling|Assembly cells balancing|NaN|NaN|NaN|NaN|NaN
+58|Assembly|Groups assembly (stand-alone)|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+59|Assembly|Groups assembly (stand-alone)|Assembly centerline creation|NaN|NaN|NaN|NaN|NaN
+60|Assembly|Groups assembly (stand-alone)|Top 5 quality issues (design components)|NaN|NaN|NaN|NaN|NaN
+61|Assembly|Modules stan-alone assembly/wiring and final installation on framework|Sequence validation and line balancing update|NaN|NaN|NaN|NaN|NaN
+62|Assembly|Modules stan-alone assembly/wiring and final installation on framework|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+63|Assembly|Cabinet testing|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+64|Assembly|Cabinet testing|Testing protocol and testing tools definition|NaN|NaN|NaN|NaN|NaN
+65|Assembly|Machine completion (modules and wiring)|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+66|Assembly|Machine completion (modules and wiring)|Assembly centerline creation|NaN|NaN|NaN|NaN|NaN
+67|Assembly|Machine completion (modules and wiring)|Maintaining operations evaluation|NaN|NaN|NaN|NaN|NaN
+68|Assembly|Machine guards installation|Safety visual check|NaN|NaN|NaN|NaN|NaN
+69|Assembly|Machine transport to testing areas|Top 3 losses (JIG application/removal)|NaN|NaN|NaN|NaN|NaN
+70|Testing and acceptance|Machines and layout placement and connection|Installation and sequence analisys and optimization|NaN|NaN|NaN|NaN|NaN
+71|Testing and acceptance|Machines and layout placement and connection|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+72|Testing and acceptance|Power on & dry run|Machine standard adjustment and testing protocol definition|NaN|NaN|NaN|NaN|NaN
+73|Testing and acceptance|Power on & dry run|Sequential testing hypothesis definition|NaN|NaN|NaN|NaN|NaN
+74|Testing and acceptance|Power on & dry run|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+75|Testing and acceptance|Power on & dry run|Dry run centerline creation|NaN|NaN|NaN|NaN|NaN
+76|Testing and acceptance|Materials unwinding|Parameters list and centerline definition/completion|NaN|NaN|NaN|NaN|NaN
+77|Testing and acceptance|Materials unwinding|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+78|Testing and acceptance|Wrapping tests|Parameters list and centerline definition/completion|NaN|NaN|NaN|NaN|NaN
+79|Testing and acceptance|Wrapping tests|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+80|Testing and acceptance|Final quality checks, PreFAT and FAT|HMI analysis & feedback|NaN|NaN|NaN|NaN|NaN
+81|Testing and acceptance|Final quality checks, PreFAT and FAT|Testing centerline creation|NaN|NaN|NaN|NaN|NaN
+82|Testing and acceptance|Final quality checks, PreFAT and FAT|Product quality report|NaN|NaN|NaN|NaN|NaN
+83|Testing and acceptance|Final quality checks, PreFAT and FAT|Machine status |NaN|NaN|NaN|NaN|NaN
+84|Testing and acceptance|Disconnection and Shipment|Top 5 losses & cost update|NaN|NaN|NaN|NaN|NaN
+85|Testing and acceptance|Disconnection and Shipment|Shipment centerline creation|NaN|NaN|NaN|NaN|NaN
+86|Final design definition|Final design definition|Final design review and cost update|NaN|NaN|NaN|NaN|NaN
+87|Final design definition|Change request analysis|Change requests filter and prioritization|NaN|NaN|NaN|NaN|NaN
+88|Final BOM (engineering,assembly and sevices)|Final BOM (engineering,assembly and sevices)|Ebom completion|NaN|NaN|NaN|NaN|NaN
+89|Installation & SAT - Market launch|Installation|Top 5 installation losses |NaN|NaN|NaN|NaN|NaN
+90|Installation & SAT - Market launch|Commissioning|Troubleshooting database creation|NaN|NaN|NaN|NaN|NaN
+91|Installation & SAT - Market launch|SAT|Product quality report and wrapping material specification|NaN|NaN|NaN|NaN|NaN
+92|Installation & SAT - Market launch|SAT|Machine digital identity card creation|NaN|NaN|NaN|NaN|NaN
+93|Final BOM (engineering,assembly and sevices)|Final BOM (engineering,assembly and sevices)|Mbom/Sbom creation and sequencing update|NaN|NaN|NaN|NaN|NaN
+94|Handover|Project retrospective|Structured Team Retrospective|NaN|NaN|NaN|NaN|NaN
+
+Sheet: Foglio2
+[9 rows x 3 columns]
+Column names: |Role|Name|Email
+0|Head of Project Controlling|Martina Mondelli|martina.mondelli@gfe.it
+1|Head of Engineering|Mattia Meccagni|mattia.meccagni@gidi.it
+2|Industrialization Manager|Filippo Menichelli|filippo.menichelli@emmeci.it
+3|Operations Project Leader|Cristian Rossi|cristian.rossi@gidi.it
+4|Product Owner|Simone Frontini|simone.frontini@systemceramics.com
+5|Head of Project Management|Carlos Garcia Fernandez|carlos.garciaf@volpak.com
+6|Assembly Project Leader|Daniele Grosso|daniele.grosso@gidi.it
+7|Testing Project Leader|Giada Magherini|giad.magherini@gidi.it
+8|Installation Project Leader|Marco Pavesi|marco.pavesi@gidi.it
+
